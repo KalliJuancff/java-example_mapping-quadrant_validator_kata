282c89d62a2941c9a49bf30183b443dc6f356a44
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-10-27 08:14:02 +0100

    feat: take as valid first quadrant

---
+++ src/main/java/QuadrantValidatorKata/QuadrantValidator.java
@@ -0,0 +1,10 @@
+package QuadrantValidatorKata;
+
+public class QuadrantValidator {
+    public QuadrantValidator(int quadrant) {
+    }
+
+    public boolean isValid() {
+        return true;
+    }
+}
---
+++ src/test/java/QuadrantValidatorKata/QuadrantValidatorShould.java
@@ -0,0 +1,18 @@
+package QuadrantValidatorKata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QuadrantValidatorShould {
+    private static final int FIRST = 1;
+
+    @Test
+    public void take_as_valid_first_quadrant() {
+        assertThat(new QuadrantValidator(FIRST).isValid()).isTrue();
+    }
+}
+
+
+// { 1 } -> True
+// {2, 3, 4} -> False
