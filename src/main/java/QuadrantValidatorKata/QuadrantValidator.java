217b386ccb928d9dab03a0ef4f34b7c78908b5f4
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-10-27 08:52:04 +0100

    refactor(SUT): simplify implementation of method 'isValid'

--- src/main/java/QuadrantValidatorKata/QuadrantValidator.java
+++ src/main/java/QuadrantValidatorKata/QuadrantValidator.java
@@ -1,15 +1,9 @@
 package QuadrantValidatorKata;
 
 public class QuadrantValidator {
-    private static final int SECOND = 2;
-    private static final int THIRD = 3;
-    private static final int FOURTH = 4;
+    private static final int FIRST = 1;
 
     public boolean isValid(int quadrant) {
-        if (quadrant == SECOND || quadrant == THIRD || quadrant == FOURTH) {
-            return false;
-        }
-
-        return true;
+        return quadrant == FIRST;
     }
 }
